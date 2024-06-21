d1f364d30fe94796be115c3dbf6063c88d440919
kannappan <kannappan@colanonline.com>
2024-06-21 16:26:09 +0530

    62

--- Middleware/ErrorHandler.cs
+++ Middleware/ErrorHandler.cs
@@ -1,15 +1,22 @@
+using System.Net;
+using Newtonsoft.Json;
+
 namespace BackEnd.Middleware
 {
     public class ErrorHandler
     {
         private readonly RequestDelegate _next;
-       public ErrorHandler(RequestDelegate next)
-       {
-        _next = next;   
-       } 
+        public ErrorHandler(RequestDelegate next)
+        {
+            _next = next;
+        }
 
-       public async Task Invoke (HttpContext context){
-        
-       }
+        public async Task Invoke(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = JsonConvert.SerializeObject(new { error = exception.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)(code);
+        }
     }
 }
